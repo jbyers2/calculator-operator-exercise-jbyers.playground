6a0ade0e4779ac8e9bde8711c78dcce67b1ffd4a
jbyers2 <jbyers2@gmail.com>
2016-07-11 16:29:47 -0500

    jbyers

---
+++ Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//input variables
+var i1:Float = 2
+var i2:Float = 4
+let pct:Float = 0.01
+
+//results
+i1+i2
+i1-i2
+i1*i2
+i1/i2
+i1*i2*pct
